34a8605c223d183c68a9ed2fdf99909f0f7e6f7a
analizajb <96861913+analizajb@users.noreply.github.com>
2022-05-31 23:38:56 -0700

    creating mySQL queries

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 const express = require('express');
 
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your SQL username,
+        user: 'root',
+        // Your SQL password
+        password: 'Ru$hed1995Ok',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);  
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+    VALUES (?,?,?,?)`;
+
+const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err,result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 // Default response for any other request (not found)
 app.use((req, res) => {
