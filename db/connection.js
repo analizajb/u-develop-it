405e99b86443cbda460159d3c8869dac8ea7e51b
analizajb <96861913+analizajb@users.noreply.github.com>
2022-06-10 17:23:18 -0700

    Modularize the Application

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'Ru$hed1995Ok',
+  database: 'election'
+});
+module.exports = db;
